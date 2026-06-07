8f4d058a9e51c6bc23cdc03f8a5ac7d26728df61
Parth-1104 <141064865+Parth-1104@users.noreply.github.com>
2026-06-08 01:13:09 +0530

    feat: base model done testing working

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,9 @@ const { connectDb } = require('./db.js');
 
 
 app.use(cors({
-    origin: '*', 
-    credentials: true
-  }));
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 
 
